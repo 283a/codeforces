656621b8323a4e9ee328f8546cfd44be38d02f35
jonas <jonas.haug@stud.tu-darmstadt.de>
2026-03-27 22:15:39 +0100

    112A

---
+++ problems/112A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    for (char &c : s1)
+        c = tolower(c);
+    for (char &c : s2)
+        c = tolower(c);
+
+    for (int i = 0; i < s1.size(); i++)
+    {
+        if (s1[i] < s2[i])
+        {
+            cout << -1 << endl;
+            return 0;
+        }
+        if (s1[i] > s2[i])
+        {
+            cout << 1 << endl;
+            return 0;
+        }
+    }
+    cout << 0 << endl;
+
+    // cout << s1 << s2 << endl;
+
+    return 0;
+}
